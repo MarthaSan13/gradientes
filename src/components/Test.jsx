bca2b2fa621581362293db27c6bf2d93415e8791
MarthaSan13 <u1701456@unimilitar.edu.co>
2022-06-21 16:39:48 -0500

    colors

---
+++ src/components/Test.jsx
@@ -0,0 +1,48 @@
+import React, { Fragment, useState, useEffect } from 'react';
+
+const Test = () => {
+
+    return (
+        <Fragment>
+            <div className='outCircle'>
+                <div className='firtsCircle Circle1'></div>
+                <div className='firtsCircle Circle2'></div>
+                <div className='firtsCircle Circle3'></div>
+                <div className='firtsCircle Circle4'></div>
+                <div className='firtsCircle Circle5'></div>
+                <div className='firtsCircle Circle6'></div>
+                <div className='firtsCircle Circle7'></div>
+                <div className='firtsCircle Circle8'></div>
+                <div className='firtsCircle Circle9'></div>
+                <div className='firtsCircle Circle10'></div>
+                <div className='firtsCircle Circle11'></div>
+                <div className='firtsCircle Circle12'></div>
+                <div className='firtsCircle Circle13'></div>
+                <div className='firtsCircle Circle14'></div>
+                <div className='firtsCircle Circle15'></div>
+                <div className='firtsCircle Circle16'></div>
+                <div className='firtsCircle Circle17'></div>
+                <div className='firtsCircle Circle18'></div>
+                <div className='firtsCircle Circle19'></div>
+                <div className='firtsCircle Circle20'></div>
+                <div className='firtsCircle Circle21'></div>
+                <div className='firtsCircle Circle22'></div>
+                <div className='firtsCircle Circle23'></div>
+                <div className='firtsCircle Circle24'></div>
+                <div className='firtsCircle Circle25'></div>
+                <div className='firtsCircle Circle26'></div>
+                <div className='firtsCircle Circle27'></div>
+                <div className='firtsCircle Circle28'></div>
+                <div className='firtsCircle Circle29'></div>
+                <div className='firtsCircle Circle30'></div>
+                <div className='firtsCircle Circle41'></div>
+                <div className='firtsCircle Circle42'></div>
+                <div className='firtsCircle Circle43'></div>
+                <div className='firtsCircle Circle44'></div>
+            </div>
+        </Fragment>
+    )
+
+}
+
+export default Test;
